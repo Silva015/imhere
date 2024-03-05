80bec7aa64be3fb86bd803d9eb6f822dd1d61bc9
Silva015 <tutuscarneiro@gmail.com>
2024-03-05 11:34:13 -0300

    Adiciona tela inicial e estilos para o aplicativo

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sábado, 2 de março de 2024</Text>
+    </View>
+  );
+}
