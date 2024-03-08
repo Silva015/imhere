41092d2d7222339ea8a279250ba2c1b2cb644328
Silva015 <tutuscarneiro@gmail.com>
2024-03-08 09:50:18 -0300

    Adiciona componente Participant e estilos na tela Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,24 +2,38 @@ import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { styles } from "./styles";
 
+import { Participant } from "../../components/Participant";
+
 export function Home() {
+
   function handleAddParticipant() {
     console.log("Adicionando participante");
+  }
+
+  function handleRemoveParticipant(name: string) {
+    console.log(`Removendo participante ${name}`);
   }
 
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Sábado, 2 de março de 2024</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do Participante"
-        placeholderTextColor={"#6B6B6B"}
-        // keyboardType="email-address" É possível selecionar qual o tipo de teclado que será exibido para facilitar a vida do usuário
-      />
-      <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do Participante"
+          placeholderTextColor={"#6B6B6B"}
+          // keyboardType="email-address" É possível selecionar qual o tipo de teclado que será exibido para facilitar a vida do usuário
+        />
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Participant name="Rodrigo" onRemove={() => handleRemoveParticipant("Rodrigo")} />
+      <Participant name="Diego" onRemove={()=> handleRemoveParticipant("Diego")} />
+      <Participant name="Vini" />
+      <Participant name="Biro" />
     </View>
   );
 }
